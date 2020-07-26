966dccabdeb5eaf69714a2e22ad857d87bcd8d96
Percy Ghenburg <41657434+WriterArtistCoder@users.noreply.github.com>
2020-07-26 14:20:07 -0700

    🗑 Continued purging of thy lines unnecessary
    Thy code unnecessary will be squashed underfoot 😲🦶

--- src/main/java/fi/jari/vl53l0x_bundle/VL53L0XData.java
+++ src/main/java/fi/jari/vl53l0x_bundle/VL53L0XData.java
@@ -6,9 +6,6 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.pi4j.io.i2c.I2CBus;
-import com.pi4j.io.i2c.I2CDevice;
-
 /**
  * @author Nakki
  *
@@ -30,14 +27,10 @@ public class VL53L0XData implements Runnable {
 
 	@Override
 	public void run() {
-
-//		I2CBus bus = null;
 		VL53L0XDevice sensor = null;
 		try {
-//			bus = I2CFactory.getInstance(I2CBus.BUS_1);
 			sensor = new VL53L0XDevice(0x29, 30);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -66,21 +59,8 @@ public class VL53L0XData implements Runnable {
 		try {
 			Thread.sleep(time);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	private void getDevices(I2CBus bus) throws IOException {
-		Integer[] addresses = { 24, 25, 26, 27, 34, 35, 36, 37 };
-		List<I2CDevice> devices = new ArrayList<>();
-		for (Integer address : addresses) {
-			devices.add(bus.getDevice(address));
-		}
-		System.out.println(devices);
-		I2CDevice test = devices.get(0);
-		System.out.println(test.read());
-		test.write((byte) 11, (byte) 1);
 	}
 
 	public void stop() {
